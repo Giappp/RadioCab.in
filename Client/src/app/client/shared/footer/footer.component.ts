48f66a547eeec26ba9ca65bb0b0cf609736106dc
thanhleX <145445640+thanhleX@users.noreply.github.com>
2024-03-22 15:16:05 +0700

    .

--- Client/src/app/client/shared/footer/footer.component.ts
+++ Client/src/app/client/shared/footer/footer.component.ts
@@ -9,4 +9,8 @@ export class FooterComponent {
   address: string = '123 Đường ABC, Thành phố XYZ';
   email: string = 'info@example.com';
   phone: string = '+84912345678';
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
